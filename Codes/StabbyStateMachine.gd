c575c7c2cd92d5b1f04afb2ca8b47aae21ee1333
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2021-06-03 13:25:16 +1200

    Imported Animated model to godot v1

--- Codes/StabbyStateMachine.gd
+++ Codes/StabbyStateMachine.gd
@@ -1,18 +1,28 @@
 extends Spatial
 
-
-
 enum {
 	IDLE,
-	ALERT,
 	ATTACK,
 	STABBED,
+	WALK,
+	RUN,
 	DEAD
 } 
 
 var state = IDLE
+var otherstate = WALK
 
 onready var raycast = $RayCast
+onready var ap = $AnimationPlayer
 
 func _ready():
 	pass # Replace with function body.
+
+func _process(delta):
+	match state:
+		IDLE:
+			ap.play("Idle")
+		WALK:
+			ap.play("Walk")
+		RUN:
+			ap.play("Run")
