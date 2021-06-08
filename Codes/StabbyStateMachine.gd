e1817b035068ae8d586140fe9398d249723f4c4d
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2021-06-09 09:57:51 +1200

    Reimported stabby enemy and further completed state machine

--- Codes/StabbyStateMachine.gd
+++ Codes/StabbyStateMachine.gd
@@ -19,6 +19,14 @@ func _ready():
 	pass # Replace with function body.
 
 func _process(delta):
+	
+	if raycast.is_colliding():
+		state = WALK
+	elif Input.is_action_just_released("attack"):
+		state = STABBED
+	else:
+		state = IDLE
+	
 	match state:
 		IDLE:
 			ap.play("Idle")
@@ -26,3 +34,9 @@ func _process(delta):
 			ap.play("Walk")
 		RUN:
 			ap.play("Run")
+		ATTACK:
+			ap.play("Stabby")
+		STABBED:
+			ap.play("Get_Stabbed")
+		DEAD:
+			ap.play("")
