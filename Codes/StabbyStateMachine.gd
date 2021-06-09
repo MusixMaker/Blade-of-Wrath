48fc191fd76bf09ec867e61d8764a11c604669f8
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2021-06-10 11:00:28 +1200

    Continued State Machine

--- Codes/StabbyStateMachine.gd
+++ Codes/StabbyStateMachine.gd
@@ -4,39 +4,43 @@ enum {
 	IDLE,
 	ATTACK,
 	STABBED,
-	WALK,
 	RUN,
 	DEAD
 } 
 
 var state = IDLE
-var otherstate = WALK
+
+var target
+
+const TURN_SPEED = 2
 
 onready var raycast = $RayCast
 onready var ap = $AnimationPlayer
+onready var eyes = $Eyes
 
 func _ready():
-	pass # Replace with function body.
+	pass
+
+func _on_Area_body_entered(body):
+	if body.is_in_group("Player"):
+		state = RUN
+		target = body
+
+func _on_SightRange_body_exited(body):
+	state = IDLE
 
 func _process(delta):
-	
-	if raycast.is_colliding():
-		state = WALK
-	elif Input.is_action_just_released("attack"):
-		state = STABBED
-	else:
-		state = IDLE
-	
 	match state:
 		IDLE:
 			ap.play("Idle")
-		WALK:
-			ap.play("Walk")
 		RUN:
 			ap.play("Run")
+			eyes.look_at(target.global_transform.origin, Vector3.UP)
+			rotate_y(deg2rad(-eyes.rotation.y * TURN_SPEED))
 		ATTACK:
-			ap.play("Stabby")
-		STABBED:
-			ap.play("Get_Stabbed")
-		DEAD:
-			ap.play("")
+			ap.play("Att")
+
+
+
+func _on_StabTimer_timeout():
+	state = ATTACK
