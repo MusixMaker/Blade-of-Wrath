e39c6c3426a59411c910cb323c414e58c3573ee0
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2021-08-06 12:25:43 +1200

    Continued title screen

--- Codes/TitleButtons.gd
+++ Codes/TitleButtons.gd
@@ -1,19 +1,25 @@
 extends Control
+onready var ap = $MenuContainer/Fade/FadePlayer
+var fadequit = false
+var fadegame = false
 
 func _ready():
-	pass # Replace with function body.
+	ap.play("FadeIn")
 
 
 func _on_Play_pressed():
-	get_tree().change_scene('res://Scenes/Test Level.tscn')
-
-
+	ap.play("FadeOut")
+	fadegame = true
 
 func _on_Options_pressed():
 	pass # Replace with function body.
 
-
-
-
 func _on_Quit_pressed():
-	get_tree().quit()
+	ap.play("FadeOut")
+	fadequit = true
+
+func _on_FadePlayer_animation_finished(FadeOut):
+	if fadequit == true:
+		get_tree().quit()
+	if fadegame == true:
+		get_tree().change_scene('res://Scenes/Test Level.tscn')
