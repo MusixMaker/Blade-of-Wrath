05acc6df4cfbc9130e5e28ea4d00952b1e06a52d
MusixMaker <82478364+MusixMaker@users.noreply.github.com>
2021-08-12 09:55:19 +1200

    Added some ambient music

--- Codes/TitleButtons.gd
+++ Codes/TitleButtons.gd
@@ -1,14 +1,18 @@
 extends Control
 onready var ap = $MenuContainer/Fade/FadePlayer
+onready var map = $AudioStreamPlayer/Musicfader
 var fadequit = false
 var fadegame = false
 
 func _ready():
 	ap.play("FadeIn")
+	map.play("FadeMusicIn")
+	$AudioStreamPlayer.play()
 
 
 func _on_Play_pressed():
 	ap.play("FadeOut")
+	map.play("FadeMusicOut")
 	fadegame = true
 
 func _on_Options_pressed():
@@ -16,6 +20,7 @@ func _on_Options_pressed():
 
 func _on_Quit_pressed():
 	ap.play("FadeOut")
+	map.play("FadeMusicOut")
 	fadequit = true
 
 func _on_FadePlayer_animation_finished(FadeOut):
